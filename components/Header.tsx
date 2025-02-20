4a9fb8106a7fcca7afc4cd2575afc353f8bbb83f
Nick Trochalakis <Chewbaccalakis@users.noreply.github.com>
2025-02-20 03:39:11 -0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,10 +21,7 @@ export default function Header() {
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <div>
-          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-            <Text size="xl" span>
-              🕒
-            </Text>
+          <a href="https://status.calliecorner.net" target="_blank">
             <Text
               size="xl"
               span
@@ -32,7 +29,7 @@ export default function Header() {
               variant="gradient"
               gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
             >
-              UptimeFlare
+              CallieCorner Status
             </Text>
           </a>
         </div>
